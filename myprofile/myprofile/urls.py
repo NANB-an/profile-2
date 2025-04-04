9a0018a9ab47260fc6a2eaeaf1766e5d206ce462
NANB-an <ivancaparas07@gmail.com>
2025-04-04 14:31:01 +0800

    urls

--- myprofile/myprofile/urls.py
+++ myprofile/myprofile/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myprofileApp.urls')), 
+    path('', include('myprofile.myprofileApp.urls')), 
 ]
